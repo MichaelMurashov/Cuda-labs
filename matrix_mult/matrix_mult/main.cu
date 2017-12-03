0961da2e2bc4848997b7b12faa392bc5c8f94866
Michael Murashov <happy-est@ya.ru>
2017-12-03 13:12:39 +0300

    add shared gpu

--- matrix_mult/matrix_mult/main.cu
+++ matrix_mult/matrix_mult/main.cu
@@ -10,23 +10,71 @@ using namespace std;
 const int block_size = 16;
 
 __global__
+void shared_kernel(int n, const float *a, const float *b, float *c) {
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    int aBegin = n * block_size * by;
+    int aEnd = aBegin + n - 1;
+    int bBegin = block_size * bx;
+    int bStep = block_size * n;
+
+    int aStep = block_size;
+
+    float sum = 0.0f;
+
+    for (int ia = aBegin, ib = bBegin; ia <= aEnd; ia += aStep, ib += bStep) {
+        __shared__ float as[block_size][block_size];
+        __shared__ float bs[block_size][block_size];
+
+        as[ty][tx] = a[ia + n * ty + tx];
+        bs[ty][tx] = b[ib + n * ty + tx];
+
+        __syncthreads();
+
+        for (int k = 0; k < block_size; k++)
+            sum += as[ty][k] * bs[k][tx];
+
+        __syncthreads();
+    }
+
+    int ic = n * block_size * by + block_size * bx;
+
+    c[ic + n * ty + tx] = sum;
+}
+
+float sharedMultMat(int n, const float *dev_a, const float *dev_b, float *dev_c) {
+    dim3 threads(block_size, block_size);
+    dim3 blocks(n / threads.x, n / threads.y);
+
+    clock_t start = clock();
+
+    shared_kernel <<< blocks, threads >>> (n, dev_a, dev_b, dev_c);
+    cudaDeviceSynchronize();
+
+    clock_t finish = clock();
+
+    return (float)(finish - start) / CLOCKS_PER_SEC;
+}
+
+__global__
 void kernel(int n, const float *a, const float *b, float *c) {
-    int   bx = blockIdx.x;     // block index
+    int   bx = blockIdx.x;
     int   by = blockIdx.y;
-    int   tx = threadIdx.x;        // thread index
+    int   tx = threadIdx.x;
     int   ty = threadIdx.y;
-    float sum = 0.0f;           // computed subelement
-    int   ia = n * block_size * by + n * ty;   // a [i][0]
+
+    float sum = 0.0f;
+
+    int   ia = n * block_size * by + n * ty;
     int   ib = block_size * bx + tx;
 
-    // Multiply the two matrices together;
     for (int k = 0; k < n; k++)
         sum += a[ia + k] * b[ib + k*n];
 
-    // Write the block sub-matrix to global memory;
-    // each thread writes one element
     int ic = n * block_size * by + block_size * bx;
-
     c[ic + n * ty + tx] = sum;
 }
 
@@ -71,7 +119,7 @@ int main() {
     const int n = 64;
 
     float *a = new float[n * n], *b = new float[n * n], *c = new float[n * n];
-    float *resultGPU = new float[n * n];
+    float *resultGPU = new float[n * n], *resultSharedGPU = new float[n * n];
 
     float *dev_a, *dev_b, *dev_c;
 
@@ -94,6 +142,10 @@ int main() {
 
     cudaMemcpy(resultGPU, dev_c, n * n * sizeof(float), cudaMemcpyDeviceToHost);
 
+    float sharedGpuTime = sharedMultMat(n, dev_a, dev_b, dev_c);
+
+    cudaMemcpy(resultSharedGPU, dev_c, n * n * sizeof(float), cudaMemcpyDeviceToHost);
+
     //for (int i = 0; i < n * n; i++)
     //    cout << c[i] << ' ';
     //cout << endl;
@@ -104,11 +156,18 @@ int main() {
 
     for (int i = 0; i < n * n; i++)
         if (c[i] != resultGPU[i]) {
-            cout << "Matrixs are not equal!" << endl;
+            cout << "Matrixs cpu and gpu are not equal!" << endl;
+            break;
+        }
+
+    for (int i = 0; i < n * n; i++)
+        if (c[i] != resultSharedGPU[i]) {
+            cout << "Matrixs cpu and shared_gpu are not equal!" << endl;
             break;
         }
 
     cout << "gpu time: " << gpuTime << endl
+        << "shared gpu time: " << sharedGpuTime << endl
         << "cpu time: " << cpuTime << endl;
 
     cudaFree(dev_a); cudaFree(dev_b); cudaFree(dev_c);
